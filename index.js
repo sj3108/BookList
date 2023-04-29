5f6ee7b0e9441995a01f9cf0b9b73a862b84a632
sj3108 <sj31082002@gmail.com>
2023-04-29 22:50:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/src/index.js"));
+    res.sendFile(path.resolve(__dirname, "./build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
