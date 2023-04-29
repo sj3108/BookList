bff03e0139388094a4bee9b16539df2e7238f7d8
sj3108 <sj31082002@gmail.com>
2023-04-30 00:09:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     });
     console.log(
       `Connected To MongoDDb Database ${mongoose.connection.host}`.bgGreen
