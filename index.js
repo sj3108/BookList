dada716787225d1789049996f24380b694eb346c
sj3108 <sj31082002@gmail.com>
2023-04-29 23:04:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/api/auth", authRoute);
 // ----------deployment----------------
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./build/index.html"));
   });
